c50e032e229a34c538cf4081234533c7196c3069
danielalvespinheiro <danielalvespinheiro467@gmail.com>
2024-05-18 10:17:21 -0400

    criação do repositório

---
+++ src/pack/Aula39.java
@@ -0,0 +1,20 @@
+package pack;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Aula39 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double preco = 34.5;
+		
+		double resultado = (preco < 20) ? preco * 0.1 : preco * 0.05;
+		
+		System.out.println("O valor da compra foi: " + resultado);
+		sc.close();
+	}
+
+}
---
+++ src/pack/Aula49_1.java
@@ -0,0 +1,12 @@
+package pack;
+
+public class Aula49_1 {
+
+public static void main(String[] args) {
+		
+		
+		for(int i=0; i<1001; i++) {
+			System.out.println("valor de i " + i);
+		}
+	}
+}
